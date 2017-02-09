60a93633e80123d76f55ea84bc2ea49f511d4f4a
Dragon Controller <Dragon Controller@10.8.81.179>
2017-02-08 18:11:06 -0800

    initial commit

---
+++ Getting Started/src/org/usfirst/frc/team2813/robot/Robot.java
@@ -0,0 +1,67 @@
+package org.usfirst.frc.team2813.robot;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	RobotDrive myRobot = new RobotDrive(0, 1, 2, 3);
+	Joystick stick = new Joystick(0);
+	Timer timer = new Timer();
+	ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+	double gyroAngle = 0;
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+	}
+
+	/**
+	 * This function is run once each time the robot enters autonomous mode
+	 */
+	public void autonomousInit() {
+		timer.reset();
+		timer.start();
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+	}
+
+	/**
+	 * This function is called once each time the robot enters tele-operated
+	 * mode.
+	 */
+	public void teleopInit() {
+		gyro = new ADXRS450_Gyro();
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		gyroAngle = gyro.getAngle();
+		myRobot.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getTwist(), 0);
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+}
